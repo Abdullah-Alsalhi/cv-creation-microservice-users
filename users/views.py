079ffab45a52c2e9ec3cb237dc2aaf4264d5affc
Abdullah-Alsalhi <abuhamed19@gmail.com>
2023-03-17 04:06:42 +0300

    solved hashing issue

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 # from django.shortcuts import render
+# from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
+# from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
 from rest_framework.parsers import JSONParser
-# from django.contrib.auth.models import User
 from .models import User
 from .serializer import SerializerUser
 from django.contrib.auth import authenticate
@@ -12,6 +13,45 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import AccessToken
 # Create your views here.
+# import bcrypt
+
+
+# def hash_password(password) -> str:
+
+#     encoded_password = password.encode('utf-8')
+#     salt = bcrypt.gensalt(10)  # Generate a salt
+#     # Hash the password with the salt
+#     return bcrypt.hashpw(encoded_password, salt)
+
+@api_view(['POST'])
+def register(request: Request) -> Response:
+    """ register a new account """
+    data = JSONParser().parse(request)
+    serializer = SerializerUser(data=data)
+    if serializer.is_valid():
+        user = serializer.create(data)
+        return Response({"msg": f"created {user.email}"}, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def login(request: Request) -> Response:
+    data = JSONParser().parse(request)
+    serializer = SerializerUser(data=data)
+
+    username = str(request.data.get("username"))
+    password = str(request.data.get("password"))
+    if serializer.is_valid():
+
+        user = authenticate(request, username=username, password=password)
+    # user = authenticate(request, email=email, password=password)
+    if user is None:
+
+        return Response({"msg": "user not found, please check your credentials"})
+
+    token = str(AccessToken.for_user(user=user))
+    return Response({"msg": "logged in", "token": token})
 
 
 """
@@ -39,34 +79,3 @@ def register_user(request : Request) -> Response :
 	return Response({"Error" :user.errors}, status=status.HTTP_400_BAD_REQUEST)
 
  """
-
-
-@api_view(['POST'])
-def register_user(request: Request) -> Response:
-    """ register a new account """
-
-    data = JSONParser().parse(request)
-    serializer = SerializerUser(data=data)
-
-    if serializer.is_valid():
-
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['POST'])
-def login_user(request: Request) -> Response:
-
-    email = str(request.data.get("email"))
-    password = str(request.data.get("password"))
-
-    user = authenticate(request, email=email, password=password)
-
-    if user is None:
-
-        return Response({"msg": "user not found, please check your credentials"})
-
-    token = str(AccessToken.for_user(user=user))
-    return Response({"msg": "logged in", "token": token})
